3da7872db270b956d8bb18b60e114500d624c593
Ricco Amezcua <31037633+RiccoA@users.noreply.github.com>
2022-09-13 16:53:27 -0500

    Basic editor to working as a component

---
+++ src/feature/WorkFlowEditor.tsx
@@ -0,0 +1,43 @@
+import { useWorkFlowEditorContext } from "./WorkflowEditorContext"
+import { cloneFormComponent, ComponentType } from "./workflowEditorState"
+
+export const WorkFlowEditor = () => {
+  const [
+    workflowEditorState,
+    addComponent,
+    removeComponent,
+    editComponent,
+    swapTwoComponents,
+  ] = useWorkFlowEditorContext()
+
+  const addOnClick = () => {
+    addComponent(ComponentType.Headline)
+  }
+
+  const removeOnClick = () => {
+    removeComponent(0)
+  }
+
+  const editComponentOnClick = () => {
+    var component = cloneFormComponent(
+      workflowEditorState.activeForm.components[0]
+    )
+    component.isRequired = true
+    editComponent(0, component)
+  }
+
+  const swapTwoComponentsOnClick = () => {
+    swapTwoComponents(0, 1)
+  }
+
+  return (
+    <>
+      <h1>Workflow Editor </h1>
+      <pre>{JSON.stringify(workflowEditorState, null, 2)}</pre>
+      <button onClick={addOnClick}>Add Component</button>
+      <button onClick={removeOnClick}>Remove Component</button>
+      <button onClick={editComponentOnClick}>Edit Component</button>
+      <button onClick={swapTwoComponentsOnClick}>Swap Components</button>
+    </>
+  )
+}
