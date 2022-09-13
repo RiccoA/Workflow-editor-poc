312b9936dd02061970a8e87f3c902bcc13c978f7
Ricco Amezcua <31037633+RiccoA@users.noreply.github.com>
2022-09-13 14:59:18 -0500

    remove component working with dispatcher

--- src/feature/useWorkflowEditorState.test.ts
+++ src/feature/useWorkflowEditorState.test.ts
@@ -1,19 +1,15 @@
 import { renderHook, act } from "@testing-library/react"
 import { useWorkflowEditorState } from "./useWorkflowEditorState"
-import { ComponentType } from "./WorkflowEditorState"
+import { ComponentType } from "./workflowEditorState"
 
 test("should have one component after adding", () => {
   const { result } = renderHook(() => useWorkflowEditorState())
-  expect(result.current.workflowEditorState.activeForm.components.length).toBe(
-    0
-  )
+  expect(result.current.workflowEditorState.activeForm.components.length).toBe(0)
   act(() => {
     result.current.addComponent(ComponentType.Headline)
   })
 
-  expect(result.current.workflowEditorState.activeForm.components.length).toBe(
-    1
-  )
+  expect(result.current.workflowEditorState.activeForm.components.length).toBe(1)
 })
 test("should have two components after adding two", () => {
   const { result } = renderHook(() => useWorkflowEditorState())
@@ -32,6 +28,7 @@ test("should have two components after adding two", () => {
     2
   )
 })
+
 test("should remove component from state", () => {
   const { result } = renderHook(() => useWorkflowEditorState())
 
@@ -52,49 +49,49 @@ test("should remove component from state", () => {
   )
 })
 
-test("should edit component at index", () => {
-  const { result } = renderHook(() => useWorkflowEditorState())
+// test("should edit component at index", () => {
+//   const { result } = renderHook(() => useWorkflowEditorState())
 
-  act(() => {
-    result.current.addComponent(ComponentType.Headline)
-  })
+//   act(() => {
+//     result.current.addComponent(ComponentType.Headline)
+//   })
 
-  expect(result.current.workflowEditorState.activeForm.components.length).toBe(
-    1
-  )
+//   expect(result.current.workflowEditorState.activeForm.components.length).toBe(
+//     1
+//   )
 
-  const componentToEdit =
-    result.current.workflowEditorState.activeForm.components[0]
-  componentToEdit.isRequired = true
+//   const componentToEdit =
+//     result.current.workflowEditorState.activeForm.components[0]
+//   componentToEdit.isRequired = true
 
-  act(() => {
-    result.current.editComponent(1, componentToEdit)
-  })
+//   act(() => {
+//     result.current.editComponent(1, componentToEdit)
+//   })
 
-  expect(
-    result.current.workflowEditorState.activeForm.components[1].isRequired
-  ).toBe(true)
-})
+//   expect(
+//     result.current.workflowEditorState.activeForm.components[1].isRequired
+//   ).toBe(true)
+// })
 
-test("should swap two components", () => {
-  const { result } = renderHook(() => useWorkflowEditorState())
+// test("should swap two components", () => {
+//   const { result } = renderHook(() => useWorkflowEditorState())
 
-  act(() => {
-    result.current.addComponent(ComponentType.Headline)
-  })
+//   act(() => {
+//     result.current.addComponent(ComponentType.Headline)
+//   })
 
-  act(() => {
-    result.current.addComponent(ComponentType.InputBox)
-  })
+//   act(() => {
+//     result.current.addComponent(ComponentType.InputBox)
+//   })
 
-  expect(
-    result.current.workflowEditorState.activeForm.components[0].schema.type
-  ).toBe(ComponentType.Headline)
+//   expect(
+//     result.current.workflowEditorState.activeForm.components[0].schema.type
+//   ).toBe(ComponentType.Headline)
 
-  act(() => {
-    result.current.swapTwoComponents(0, 1)
-  })
-  expect(
-    result.current.workflowEditorState.activeForm.components[1].schema.type
-  ).toBe(ComponentType.Headline)
-})
+//   act(() => {
+//     result.current.swapTwoComponents(0, 1)
+//   })
+//   expect(
+//     result.current.workflowEditorState.activeForm.components[1].schema.type
+//   ).toBe(ComponentType.Headline)
+// })
--- src/feature/useWorkflowEditorState.ts
+++ src/feature/useWorkflowEditorState.ts
@@ -1,79 +1,97 @@
 import { useState, useCallback } from "react"
+import { useImmerReducer } from "use-immer"
 import {
   FormComponent,
   WorkflowEditorState,
   ComponentIndex,
   ComponentType,
+  createFormComponent,
 } from "./workflowEditorState"
 
 export const useWorkflowEditorState = () => {
-  const [workflowEditorState, setWorkflowEditorState] =
-    useState<WorkflowEditorState>(new WorkflowEditorState())
+  const init: WorkflowEditorState = {
+    activeForm: {
+      components: [],
+    },
+  }
+  const [OLDworkflowEditorState, setWorkflowEditorState] =
+    useState<WorkflowEditorState>(init)
+
+  const [workflowEditorState, dispatch] = useImmerReducer((draft, action) => {
+    switch (action.type) {
+      case "add":
+        draft.activeForm.components.push(
+          createFormComponent(action.payload.type)
+        )
+        break
+      case "remove":
+        draft.activeForm.components.splice(action.payload.indexToRemoveAt, 1)
+        break
+      case "swapTwo":
+        break
+      case "edit":
+        break
+      default:
+        break
+    }
+  }, init)
 
   const addComponent = useCallback(
     (type: ComponentType, index: ComponentIndex = 0) => {
-      const newState = new WorkflowEditorState()
-      workflowEditorState.activeForm.components.forEach((component) =>
-        newState.activeForm.addComponent(component.schema.type)
-      )
-
-      newState.activeForm.addComponent(type)
-
-      setWorkflowEditorState(newState)
+      dispatch({ type: "add", payload: { type } })
     },
-    [workflowEditorState.activeForm.components]
+    [dispatch]
   )
 
   const removeComponent = useCallback(
     (indexToRemoveAt: ComponentIndex) => {
-      const newState = new WorkflowEditorState()
-      workflowEditorState.activeForm.components.forEach(
-        (component, index) =>
-          indexToRemoveAt !== index ??
-          newState.activeForm.addComponent(component.schema.type)
-      )
-      setWorkflowEditorState(newState)
+      dispatch({ type: "remove", payload: { indexToRemoveAt } })
+      // const newState = new WorkflowEditorState()
+      // OLDworkflowEditorState.activeForm.components.forEach(
+      //   (component, index) =>
+      //     indexToRemoveAt !== index ??
+      //     newState.activeForm.addComponent(component.schema.type)
+      // )
+      // setWorkflowEditorState(newState)
     },
-    [workflowEditorState.activeForm.components]
+    [dispatch]
   )
 
-  const swapTwoComponents = useCallback(
-    (primaryIndex: ComponentIndex, secondaryIndex: ComponentIndex) => {
-      const newState = new WorkflowEditorState()
-      workflowEditorState.activeForm.components.forEach((component) =>
-        newState.activeForm.addComponent(component.schema.type)
-      )
+  // const swapTwoComponents = useCallback(
+  //   (primaryIndex: ComponentIndex, secondaryIndex: ComponentIndex) => {
+  //     const newState = new WorkflowEditorState()
+  //     OLDworkflowEditorState.activeForm.components.forEach((component) =>
+  //       newState.activeForm.addComponent(component.schema.type)
+  //     )
 
-      const primaryComponent =
-        workflowEditorState.activeForm.getComponent(primaryIndex)
-      const secondaryComponent =
-        workflowEditorState.activeForm.getComponent(secondaryIndex)
+  //     const primaryComponent =
+  //       OLDworkflowEditorState.activeForm.getComponent(primaryIndex)
+  //     const secondaryComponent =
+  //       OLDworkflowEditorState.activeForm.getComponent(secondaryIndex)
 
-      newState.activeForm.setComponent(primaryIndex, secondaryComponent)
-      newState.activeForm.setComponent(secondaryIndex, primaryComponent)
+  //     newState.activeForm.setComponent(primaryIndex, secondaryComponent)
+  //     newState.activeForm.setComponent(secondaryIndex, primaryComponent)
 
-      setWorkflowEditorState(newState)
-    },
-    [workflowEditorState.activeForm]
-  )
+  //     setWorkflowEditorState(newState)
+  //   },
+  //   [OLDworkflowEditorState.activeForm]
+  // )
 
-  const editComponent = useCallback(
-    (indexToEditAt: ComponentIndex, component: FormComponent) => {
-      const newState = new WorkflowEditorState()
-      workflowEditorState.activeForm.components.forEach((component) =>
-        newState.activeForm.addComponent(component.schema.type)
-      )
-      newState.activeForm.setComponent(indexToEditAt, component)
-      setWorkflowEditorState(newState)
-    },
-    [workflowEditorState.activeForm.components]
-  )
+  // const editComponent = useCallback(
+  //   (indexToEditAt: ComponentIndex, component: FormComponent) => {
+  //     const newState = new WorkflowEditorState()
+  //     OLDworkflowEditorState.activeForm.components.forEach((component) =>
+  //       newState.activeForm.addComponent(component.schema.type)
+  //     )
+  //     newState.activeForm.setComponent(indexToEditAt, component)
+  //     setWorkflowEditorState(newState)
+  //   },
+  //   [OLDworkflowEditorState.activeForm.components]
+  // )
 
   return {
     workflowEditorState,
     addComponent,
-    editComponent,
     removeComponent,
-    swapTwoComponents,
   }
 }
